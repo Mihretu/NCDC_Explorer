8df1659af5cf5722d56a14da39e14c925b16e550
srlightfoote <srlightfoote@gmail.com>
2017-05-19 21:22:20 +0000

    rev 0.0

---
+++ server.R
@@ -0,0 +1,175 @@
+# Server
+countries<-ncdc_locs(locationcategoryid = 'CNTRY',limit=1000)$data
+states<-ncdc_locs(locationcategoryid = 'ST',limit=1000)$data
+function(input, output,session) {
+  
+  #initialize reactive values
+  active<-reactiveValues()
+  updateSelectInput(session,'country',choices=countries$name,selected='United States')
+  updateSelectInput(session,'state',choices=states$name,selected='Massachusetts')
+  state<-reactive({input$states})
+  country<-reactive({input$countries})
+  basemap<-reactive({input$basemap})
+  
+  #initialize map
+  output$map<-renderLeaflet({
+    leaflet() %>% addProviderTiles('CartoDB.Positron')  %>%
+      addMeasure(position='topleft',primaryLengthUnit = 'kilometers',secondaryLengthUnit = 'miles') %>%
+      addEasyButton(easyButton(
+        icon="fa-globe", title="Zoom to Level 3",
+        onClick=JS("function(btn, map){ map.setZoom(3); }")))
+  })
+  
+  #change basemap
+  observe({
+    tile<-basemap()
+    leafletProxy("map") %>% clearTiles() %>% addProviderTiles(provider = tile)
+  })
+  
+  #update map bounds as the map is moved
+  observe({
+    updateNumericInput(session,inputId = 'minlat',value =input$map_bounds$south)
+    updateNumericInput(session,inputId = 'maxlat',value =input$map_bounds$north)
+    updateNumericInput(session,inputId = 'minlong',value =input$map_bounds$west)
+    updateNumericInput(session,inputId = 'maxlong',value =input$map_bounds$east)
+  })
+  
+  #define the location ID, if inside the US, use the state ID else use the country ID
+  locID<-reactive({
+    if(input$country=='United States'){
+      states$id[states$name %in% input$state]
+    } else{
+      countries$id[countries$name %in% input$country]
+    }
+  })
+  
+  #query the stations within the user-defined search criteria
+  observe({
+    if(input$searchCriteria=='designation'){
+      active$stations<-reactive({ncdc_stations(datasetid = input$datasets,limit=1000,locationid = locID(),sortfield = 'name')$data})
+    } else {
+      searchExtent<-reactive({as.numeric(c(input$minlat,input$minlong,input$maxlat,input$maxlong))})
+      active$stations<-reactive({ncdc_stations(datasetid = input$datasets,limit=1000,extent =searchExtent(),sortfield = 'name')$data})
+    }
+    #show stations in a data table
+    output$stations <- DT::renderDataTable(active$stations(),rownames=FALSE,filter='top')
+    
+    #show stations in a map
+    pal <- colorNumeric(palette = input$colors, active$stations()$datacoverage)
+    labels <- sprintf(
+      "<strong>%s</strong><br/>start date: %s<br/>end date: %s",
+      active$stations()$name, active$stations()$mindate,active$stations()$maxdate) %>% 
+      lapply(htmltools::HTML)
+    
+    leafletProxy(mapId = 'map',session = session,data = active$stations()) %>% 
+      clearShapes() %>%
+      clearControls() %>%
+      addCircles(~longitude,~latitude, weight = 1,radius=input$markersize/2,
+                 fillColor =~pal(datacoverage),color = "#777777",
+                 stroke = TRUE, fillOpacity = .7,
+                 highlightOptions = highlightOptions(color = "black", weight = 3,
+                                                     bringToFront = TRUE),
+                 label=labels,
+                 labelOptions = labelOptions(
+                   style = list("font-weight" = "normal", padding = "3px 8px"),
+                   textsize = "15px",
+                   direction = "auto")
+      )%>% 
+      addLegend(position = "bottomleft",
+                pal = pal, values = ~datacoverage,title = "Data Coverage [%]")
+    
+    # zoom to designation area
+    if (input$searchCriteria=='designation'){
+      leafletProxy('map',session = session,data = active$stations()) %>% 
+        fitBounds(lat1 = ~min(latitude,na.rm=TRUE)-.01,
+                  lat2 = ~max(latitude,na.rm=TRUE)+.01,
+                  lng1 = ~min(longitude,na.rm=TRUE)-.01,
+                  lng2 = ~max(longitude,na.rm=TRUE)+.01)
+    }
+  })
+  
+  #on station select, query the available datatypes and zoom map in
+  observeEvent(input$stations_rows_selected,{
+    selected.dt<-active$stations()[input$stations_rows_selected,]
+    datatypes<-ncdc_datatypes(datasetid = input$datasets,stationid = selected.dt$id)$data
+    updateSelectInput(session,'datatypes',choices=datatypes$id,selected=datatypes$id[1])
+    
+    #update map
+    leafletProxy("map",data=selected.dt) %>%
+      fitBounds(~min(longitude,na.rm = TRUE)-.1, ~min(latitude,na.rm = TRUE)-.1,
+                ~max(longitude,na.rm = TRUE)+.1, ~max(latitude,na.rm = TRUE)+.1)
+  })
+  
+  #on map click, select station in data table and zoom map in
+  observeEvent(eventExpr = input$map_shape_click,{
+    selected.dt<-active$stations()[round(active$stations()$latitude,3)==round(input$map_shape_click$lat,3) & 
+                                     round(active$stations()$longitude,3)==round(input$map_shape_click$lng,3),]
+    datatypes<-ncdc_datatypes(datasetid = input$datasets,stationid = selected.dt$id,sortfield = 'id')$data
+    updateSelectInput(session,'datatypes',choices=datatypes$id,selected=datatypes$id[1])
+    #update map
+    leafletProxy("map",data=selected.dt) %>%
+      fitBounds(~min(longitude,na.rm = TRUE)-.1, ~min(latitude,na.rm = TRUE)-.1,
+                ~max(longitude,na.rm = TRUE)+.1, ~max(latitude,na.rm = TRUE)+.1)
+    
+    #update data table
+    data.dt<-active$stations()
+    Table.dt<-data.dt
+    selected<-which(Table.dt$id==selected.dt$id)
+    output$stations <- DT::renderDataTable({
+      DT::datatable(Table.dt,filter="top",options = list(pageLength = 10,displayStart = selected-1),selection = list(mode = "single", target = "row", selected = selected) )
+    })
+    updateTabsetPanel(session = session,inputId = 'tabs',selected = 'Tabular')
+    
+  })
+  # query and  plot user-defined data
+  observeEvent(input$queryData,{
+    
+    #calculate length of output observations and split up query accordingly (1-year and 1000 row limit to API)
+    numChannels<-length(input$datatypes)
+    dateDiff<-as.numeric(difftime(input$dateRange[2],input$dateRange[1]))
+    numObs<-dateDiff*datasets$dayMultiplier[datasets$id==input$datasets]
+    queryRows<-numChannels*numObs
+    if((queryRows>1000 | dateDiff>365) & !is.na(queryRows)){
+      queries<-expand.grid(datasetid=input$datasets,startdate=as.character(seq(input$dateRange[1],input$dateRange[2],by='year')),enddate=NA,
+                           datatypeid=input$datatypes,stationid=active$stations()$id[input$stations_rows_selected],limit=1000,stringsAsFactors = FALSE)
+      queries$enddate[year(queries$startdate)!=year(input$dateRange[2])]<-
+        paste(year(queries$startdate[year(queries$startdate)!=year(input$dateRange[2])]),'-12-31',sep='')
+      queries$enddate[is.na(queries$enddate)]<-as.character(input$dateRange[2])
+      #query sequentially
+      out.ls<-list()
+      withProgress(message = 'Querying', value = 0, {
+        for (i in 1:nrow(queries)){
+          incProgress(i/nrow(queries),message = paste('Querying ',queries$datatypeid[i],': ',queries$startdate[i],' - ',queries$enddate[i],sep=''))
+          out.ls[[i]]<-data.table(ncdc(datasetid = queries$datasetid[i],datatypeid = queries$datatypeid[i],
+                                       stationid=queries$stationid[i],startdate=queries$startdate[i],
+                                       enddate=queries$enddate[i],limit=queries$limit[i])$data)
+        }
+      })
+      #combine data
+      active$data.dt<-bind_rows(out.ls)
+      
+    }else{
+      active$data.dt<-data.table(ncdc(datasetid = input$datasets,datatypeid = input$datatypes,
+                                      startdate = as.character(input$dateRange[1]),enddate=as.character(input$dateRange[2]),
+                                      stationid = active$stations()$id[input$stations_rows_selected],limit=1000)$data)
+    }
+    
+    #if data exists, plot it
+    if(nrow(active$data.dt)>0){
+      active$data.dt$date<-as.POSIXct(active$data.dt$date,format='%Y-%m-%dT%H:%M:%S')
+      data.xts<-as.xts.data.table(spread(active$data.dt[,c('date','value','datatype'),with=FALSE],datatype,value))
+      output$plot<-renderDygraph({dygraph(data.xts,
+                                          main = paste(active$stations()$name[input$stations_rows_selected],' ',
+                                                       input$datatypes,sep='')) %>% dyRoller() %>%dyRangeSelector()})
+      updateTabsetPanel(session = session,inputId = 'tabs',selected = 'Data')
+      output$results<-renderText('query successful')
+    } else {output$results<-renderText('no data found')}
+  })
+  
+  #export data to csv
+  output$exportData <- downloadHandler(
+    filename = function() {  paste(active$stations()$name[input$stations_rows_selected],' ',input$datatypes,'_data.csv',sep='')},
+    content = function(con) { write.csv(active$data.dt, con,row.names = FALSE)}
+  )
+  
+}
---
+++ ui.R
@@ -0,0 +1,117 @@
+#load libraries
+library(shiny)
+library(shinyBS)
+library(leaflet)
+library(rnoaa)
+library(ggplot2)
+library(dygraphs)
+library(DT)
+library(tidyverse)
+library(RColorBrewer)
+library(lubridate)
+library(xts)
+library(data.table)
+#define user API key
+options(noaakey = "<your API key here>")
+
+#ignore inactive datasets
+activeStationCutoff<-'2010-01-01'
+
+#get available datasets
+datasets<-ncdc_datasets(startdate = activeStationCutoff)$data
+datasets<-datasets[!(datasets$id %in% c('NEXRAD2','NEXRAD3')),] #NEXRAD2 and NEXRAD3 don't work
+
+#define the resolution of the data as a fraction of a daily increment
+datasets$dayMultiplier<-NA
+datasets$dayMultiplier<-c(1,1/31,1/365,NA,NA,NA,NA,24*4,24)
+
+fluidPage(
+  # Application title
+  titlePanel("NCDC Explorer"),
+  sidebarLayout(
+    # Sidebar query options
+    sidebarPanel(width = 3,
+                 selectInput('datasets',label = 'Datasets',datasets$id,selected = datasets$id[1]),
+                 radioButtons('searchCriteria','search by',choices=c('designation','coordinates'),selected = 'designation',inline = TRUE),
+                 bsTooltip(id = 'searchCriteria', title = "NCDC API rate limit = 1000, if rate limit of stations is exceeded, stations will not be shown.  Zoom in to query additional stations", 
+                           placement = "right", trigger = "hover"),
+                 conditionalPanel("input.searchCriteria=='designation'",
+                                  selectInput('country','country',choices='United States'),
+                                  conditionalPanel(
+                                    condition = "input.country == 'United States'",
+                                    selectInput('state','state',choices='Massachusetts')
+                                  )
+                 ),
+                 conditionalPanel("input.searchCriteria=='coordinates'",
+                                  numericInput(inputId = 'minlat','Lat Min',value = NULL),
+                                  numericInput(inputId = 'maxlat','Lat Max',value = NULL),
+                                  numericInput(inputId = 'minlong','Lng Min',value = NULL),
+                                  numericInput(inputId = 'maxlong','Lng Max',value = NULL)
+                 ),
+                 helpText('Select a station in the data table or click on a station in the map to populate the available data types for a particular station'),
+                 selectInput('datatypes','Datatypes',choices = NULL,multiple = TRUE),
+                 dateRangeInput('dateRange',label='Date Range',start = '2010-01-01',end='2010-12-31'),
+                 bsTooltip(id = 'dateRange', title = 'NORMAL Datasets use the year 2010, all others use explicit years', 
+                           placement = "right", trigger = "hover"),
+                 actionButton('queryData','Query Data'),
+                 downloadButton('exportData','Export Data'),
+                 textOutput('results')
+    ),
+    
+    # stations returned
+    mainPanel(
+      tabsetPanel(id = 'tabs',
+                  tabPanel(title = 'Map',value = 'Map',
+                           fluidRow(column(4,sliderInput("markersize","Marker Size",min=0,max=50000,step=100,value = 5000,post = 'm')),
+                                    column(4,selectInput("colors", "Color Scheme",choices =
+                                                           c('viridis',rownames(subset(brewer.pal.info, category %in% c("seq", "div")))),
+                                                         selected ='viridis')),
+                                    column(4,selectInput('basemap','Basemap',choices = 
+                                                           c("CartoDB.Positron","CartoDB.DarkMatter", "Esri","Esri.WorldStreetMap","Esri.DeLorme","Esri.WorldTopoMap","Esri.WorldImagery",
+                                                             "Esri.WorldTerrain","Esri.WorldShadedRelief","Esri.WorldPhysical","Esri.OceanBasemap","Esri.NatGeoWorldMap" )
+                                                         ,selected = "CartoDB.Positron"))),
+                           leafletOutput('map')),
+                  tabPanel(title = 'Tabular',value='Tabular',div(DT::dataTableOutput("stations"),style = "font-size: 75%; width: 75%")),
+                  tabPanel(title = 'Data',value='Data',dygraphOutput('plot')),
+                  tabPanel(title='Citations',value='Citations',
+                           p('Scott Chamberlain (2017). rnoaa: \'NOAA\' Weather Data from R. R package version 0.7.0.
+  https://CRAN.R-project.org/package=rnoaa'),
+                           br(),
+                           p('Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan McPherson (2017). shiny: Web Application Framework
+  for R. R package version 1.0.3. https://CRAN.R-project.org/package=shiny'),
+                           br(),
+                           p('Joe Cheng, Bhaskar Karambelkar and Yihui Xie (2017). leaflet: Create Interactive Web Maps with the JavaScript
+  \'Leaflet\' Library. R package version 1.1.0. https://CRAN.R-project.org/package=leaflet'),
+                           br(),
+                           p('Hadley Wickham (2017). tidyverse: Easily Install and Load \'Tidyverse\' Packages. R package version 1.1.1.
+  https://CRAN.R-project.org/package=tidyverse'),
+                           br(),
+                           p('H. Wickham. ggplot2: Elegant Graphics for Data Analysis. Springer-Verlag New York, 2009.'),
+                           br(),
+                           p('Matt Dowle and Arun Srinivasan (2017). data.table: Extension of `data.frame`. R package version 1.10.4.
+  https://CRAN.R-project.org/package=data.table'),
+                           br(),
+                           p('Dan Vanderkam, JJ Allaire, Jonathan Owen, Daniel Gromer, Petr Shevtsov and Benoit Thieurmel (2017). dygraphs:
+  Interface to \'Dygraphs\' Interactive Time Series Charting Library. R package version 1.1.1.4.
+  https://CRAN.R-project.org/package=dygraphs'),
+                           br(),
+                           p('Eric Bailey (2015). shinyBS: Twitter Bootstrap Components for Shiny. R package version 0.61.
+  https://CRAN.R-project.org/package=shinyBS'),
+                           br(),
+                           p('Yihui Xie (2016). DT: A Wrapper of the JavaScript Library \'DataTables\'. R package version 0.2.
+  https://CRAN.R-project.org/package=DT'),
+                           br(),
+                           p('Erich Neuwirth (2014). RColorBrewer: ColorBrewer Palettes. R package version 1.1-2.
+  https://CRAN.R-project.org/package=RColorBrewer'),
+                           br(),
+                           p('Garrett Grolemund, Hadley Wickham (2011). Dates and Times Made Easy with lubridate. Journal of Statistical
+  Software, 40(3), 1-25. URL http://www.jstatsoft.org/v40/i03/.'),
+                           br(),
+                           p('Jeffrey A. Ryan and Joshua M. Ulrich (2014). xts: eXtensible Time Series. R package version 0.9-7.
+  https://CRAN.R-project.org/package=xts')
+                  )
+      )
+    )
+  )
+)
+# Server
